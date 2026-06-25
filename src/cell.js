c8b7a78e207ecd2d12cb6d22d6cbcd4dd07d8441
Thangadurai <Thangadurai.t@appsuretechnologies.com>
2026-06-25 18:29:54 +0530

    Missing commits

--- src/cell.js
+++ src/cell.js
@@ -1,35 +1,25 @@
 import React, { Component } from 'react'
-import PropTypes from 'prop-types'
 import cn from 'classnames'
 
-export default class Cell extends Component {
-  static propTypes = {
-    label: PropTypes.string,
-    level: PropTypes.string,
-    type: PropTypes.string,
-    selected: PropTypes.bool,
-    today: PropTypes.bool,
-    onClick: PropTypes.func,
-    classes: PropTypes.object,
-  }
-
-  render() {
+const Cell=React.forwardRef((props,ref)=>{
     const classes = cn(
-      this.props.classes.cell,
-      this.props.level,
-      this.props.type,
-      { selected: this.props.selected },
-      { today: this.props.today },
-      { 'outside-range': !this.props.invalid },
-    )
+    props.classes.cell,
+    props.level,
+    props.type,
+    { selected: props.selected },
+    { today: props.today },
+    { 'outside-range': !props.invalid },
+  )
 
-    return (
-      <div
-        className={classes}
-        onClick={() => this.props.onClick(this.props.moment)}
-      >
-        {this.props.label}
-      </div>
-    )
-  }
-}
+  return (
+    <div
+    ref={ref}
+      className={classes}
+      onClick={() => props.onClick(props.moment)}
+    >
+      {props.label}
+    </div>
+  )
+
+})
+    
